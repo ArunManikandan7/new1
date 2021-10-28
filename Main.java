1b6744f89e94004cab3ca0969d1039e63c09bfc7
Arun <arunmanikandan.info@gmail.com>
2021-10-28 06:44:16 +0530

    made changes

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ class Simple{
     public static void main(String args[]){
         System.out.println("Hello Java");
         System.out.println("unused");
+        System.out.println("Hello Java");
+        System.out.println("Hello Java");
     }
 }
