d7a5c598b38803176bbbb2e6af1414fdb1f9d1cc
Arun <arunmanikandan.info@gmail.com>
2021-10-28 06:46:55 +0530

    made changes

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Simple{
     public static void main(String args[]){
-        System.out.println("Hello Java");
+        System.out.println("Hello python");
         System.out.println("unused");
         System.out.println("Hello Java");
         System.out.println("Hello Java");
